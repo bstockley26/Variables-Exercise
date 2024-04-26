da5b2b6465cdd6da7428b3681a2b16f32f4f5ea8
Brooks Stockley <bstockley26@gmail.com>
2024-04-26 14:58:23 -0400

    added more variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -11,10 +11,20 @@
             int dogAge;
             dogAge = 3;
 
+            char middleInitial = 'R';
+
+            bool yes = true;
+
+            double weight = 45;
+
+            decimal foodPrice = 20;
+
             string dogBreed = "German Shorthaired Pointer";
 
 
             Console.WriteLine($"My dogs name is {dogName} and she is {dogAge} years old. She is a {dogBreed}");
+
+            Console.WriteLine($"{dogName} weights {weight} pounds.I spemd {foodPrice} dollars for her food");
         }
     }
 }
